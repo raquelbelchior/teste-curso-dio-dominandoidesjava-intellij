1e2899630fb29ed4464ecb5378cbbcd6f2399ed2
perkles <raquel_belchior95@hotmail.com>
2022-03-21 19:19:12 -0400

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+
+
+        /* int a = 5;
+        int b = 3;
+        System.out.println("Hello World " + (a+b)*/;
+    }
+}
